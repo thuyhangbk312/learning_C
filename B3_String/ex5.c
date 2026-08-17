0fef0d65f34aef00e5e218e1b2a3a821c9e2afa1
Trung <thanhtrung210502@gmail.com>
2026-08-17 09:48:12 +0700

    Add clang format

--- B3_String/ex5.c
+++ B3_String/ex5.c
@@ -5,13 +5,15 @@
 #include <stdio.h>
 #include <string.h>
 
-void splitString(char string[], char subString[], char *str1, char *str2, uint32_t *a, uint32_t *b, uint32_t *c) {
-    char *p = strstr(string, subString);
-    char *last =  NULL;
+void splitString(char string[], char subString[], char *str1, char *str2, uint32_t *a, uint32_t *b, uint32_t *c)
+{
+    char *p    = strstr(string, subString);
+    char *last = NULL;
 
-    while ((p != NULL)) {
+    while ((p != NULL))
+    {
         last = p;
-        p = strstr(p + 1, subString);
+        p    = strstr(p + 1, subString);
     }
 
     if (last == NULL)
@@ -21,7 +23,8 @@ void splitString(char string[], char subString[], char *str1, char *str2, uint32
 
     int temp = 0;
 
-    if (strcmp(subString,"a=") == 0) {
+    if (strcmp(subString, "a=") == 0)
+    {
         if (sscanf(last, "a=%d", &temp) == 1)
         {
             *a = temp;
@@ -32,35 +35,42 @@ void splitString(char string[], char subString[], char *str1, char *str2, uint32
             printf("error");
         }
     }
-    else if (strcmp(subString,"b=") == 0) {
+    else if (strcmp(subString, "b=") == 0)
+    {
         sscanf(last, "b=%d", &b);
         printf("b = %d\n", b);
     }
-    else if (strcmp(subString,"c=") == 0) {
+    else if (strcmp(subString, "c=") == 0)
+    {
         sscanf(last, "c=%d", &c);
         printf("c = %d\n", c);
     }
-    else if (strcmp(subString,"str1=") == 0) {
-        char *start = last + 6;   // bỏ qua str1="
+    else if (strcmp(subString, "str1=") == 0)
+    {
+        char *start = last + 6;  // bỏ qua str1="
         // Không có dấu " mở
-        if (last[5] != '"') {
+        if (last[5] != '"')
+        {
             printf("ERROR\n");
             return;
         }
         // Không được rỗng: str1=""
-        if (*start == '"') {
+        if (*start == '"')
+        {
             printf("ERROR\n");
             return;
         }
         // Tìm dấu " đóng
         char *end = strchr(start, '"');
         // Không có dấu " đóng
-        if (end == NULL) {
+        if (end == NULL)
+        {
             printf("ERROR\n");
             return;
         }
         // Sau dấu " đóng phải kết thúc chuỗi
-        if (*(end + 1) != '\0' && *(end + 1) != ',') {
+        if (*(end + 1) != '\0' && *(end + 1) != ',')
+        {
             printf("ERROR\n");
             return;
         }
@@ -68,27 +78,27 @@ void splitString(char string[], char subString[], char *str1, char *str2, uint32
         sscanf(last, "str1=\"%31[^\"]\"", str1);
         printf("str1 = %s\n", str1);
     }
-    else if (strcmp(subString,"str2=") == 0) {
+    else if (strcmp(subString, "str2=") == 0)
+    {
         sscanf(last, "str2=\"%63[^\"]\"", str2);
         printf("str2 = %s\n", str2);
     }
 }
 
-int main() {
+int main()
+{
     uint32_t a, b, c;
-    char str1[32];
-    char str2[64];
-    char string[200];
-    char subString[10];
+    char     str1[32];
+    char     str2[64];
+    char     string[200];
+    char     subString[10];
 
     printf("Input string: ");
     fgets(string, sizeof(string), stdin);
     string[strcspn(string, "\n")] = 0;
-    splitString(string,"a=", str1, str2, &a, &b, &c);
-    splitString(string,"b=",str1, str2, &a, &b, &c);
-    splitString(string,"c=", str1, str2, &a, &b, &c);
-    splitString(string,"str1=", str1, str2, &a, &b, &c);
-    splitString(string,"str2=", str1, str2, &a, &b, &c);
-
-
+    splitString(string, "a=", str1, str2, &a, &b, &c);
+    splitString(string, "b=", str1, str2, &a, &b, &c);
+    splitString(string, "c=", str1, str2, &a, &b, &c);
+    splitString(string, "str1=", str1, str2, &a, &b, &c);
+    splitString(string, "str2=", str1, str2, &a, &b, &c);
 }
